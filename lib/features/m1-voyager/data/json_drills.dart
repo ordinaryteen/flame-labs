d77d0c989c69d6c7a37ae6cab76bf5da8e217ebc
nafiMulyo <muhamadnafimulyo@gmail.com>
2026-02-23 08:47:25 +0700

    completed nested object drill

--- lib/features/m1-voyager/data/json_drills.dart
+++ lib/features/m1-voyager/data/json_drills.dart
@@ -2,24 +2,45 @@ import 'dart:convert';
 
 final String rawJson = '{"id":42, "name":"Nafi"}';
 
-void main() async {
-  final json = jsonDecode(rawJson);
-  final myUser = User.fromJson(json as Map<String, dynamic>);
+final String rawIdentity = '''{
+  "id": 99,
+  "name": "Nafi",
+  "company": {
+    "name": "Flame Labs",
+    "catchPhrase": "Building deep tech"
+  }
+}''';
 
-  print("User's name is: ${myUser.name}");
+void main() async {
+  final json = jsonDecode(rawIdentity);
+  final myIdentity = User.fromJson(json as Map<String, dynamic>);
+
+  print("User's name is: ${myIdentity.company.name}");
 }
 
 class User {
   final int id;
   final String name;
-  final bool? isActive; // optional
+  final Company company;
 
-  User({required this.id, required this.name, this.isActive});
+  User({required this.id, required this.name, required this.company});
 
   factory User.fromJson(Map<String, dynamic> jsonMap) {
     return User(
         id: jsonMap['id'],
         name: jsonMap['name'],
-        isActive: jsonMap['isActive']);
+        company: Company.fromJson(jsonMap['company']));
+  }
+}
+
+class Company {
+  final String name;
+  final String? catchPhrase; // optional
+
+  Company({required this.name, this.catchPhrase});
+
+  factory Company.fromJson(Map<String, dynamic> companyJson) {
+    return Company(
+        name: companyJson['name'], catchPhrase: companyJson['catchPhrase']);
   }
 }
