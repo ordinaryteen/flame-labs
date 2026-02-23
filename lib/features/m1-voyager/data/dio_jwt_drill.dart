32e1bbe30460a14d06f12a10532b7bfb7f0bcb31
nafiMulyo <muhamadnafimulyo@gmail.com>
2026-02-23 13:10:53 +0700

    completed dio jwt drill

---
+++ lib/features/m1-voyager/data/dio_jwt_drill.dart
@@ -0,0 +1,31 @@
+import 'package:dio/dio.dart';
+
+void main() async {
+  final dio = Dio();
+
+  String? jwtToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpCJ9";
+
+  dio.interceptors.add(InterceptorsWrapper(onRequest: (options, handler) {
+    if (jwtToken != null) {
+      options.headers['Authorization'] = 'Bearer $jwtToken';
+    }
+    return handler.next(options);
+  }));
+  try {
+    // The URL goes here! Not the token.
+    final response = await dio.get('https://httpbin.org/bearer');
+    print("Vault opened! Server says: ${response.data}");
+    print("Status: ${response.statusCode}");
+  } catch (e) {
+    if (e is DioException) {
+      if (e.response?.statusCode == 401) {
+        print(
+            "CRITICAL: JWT Token Invalid or Missing! Booting user to Login Screen.");
+      } else {
+        print("Other DioError: $e");
+      }
+    } else {
+      print("Unknown Error: $e");
+    }
+  }
+}
